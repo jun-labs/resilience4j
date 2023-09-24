7aa0873a321a5420ff47d97251c23d03c5503b4f
devjun10 <devjun1023@gmail.com>
2023-09-24 18:30:50 +0900

    chore: project build
    - 프로젝트 빌드

---
+++ retry-with-spring/product-api/src/main/java/project/resilience4j/retry/ProductApplication.java
@@ -0,0 +1,13 @@
+package project.resilience4j.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
