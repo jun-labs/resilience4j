01a68f018a7e410ae39b1d39dbfff2ced91aeb68
devjun10 <devjun1023@gmail.com>
2023-09-29 04:09:57 +0900

    feature: add feign configuration

---
+++ circuitbreaker/order-api/src/main/java/project/resilience4j/order/common/configuration/feign/FeignClientConfiguration.java
@@ -0,0 +1,32 @@
+package project.resilience4j.order.common.configuration.feign;
+
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import feign.Logger;
+import feign.Request;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfiguration {
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Request.Options options() {
+        return new Request.Options(5000, 5000);
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header(CONTENT_TYPE, APPLICATION_JSON_VALUE);
+            requestTemplate.header(ACCEPT, APPLICATION_JSON_VALUE);
+        };
+    }
+}
