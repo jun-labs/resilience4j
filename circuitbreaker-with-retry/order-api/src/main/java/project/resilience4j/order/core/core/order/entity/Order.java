9e7c127050b3960d8d89016b521d6754170663de
devjun10 <devjun1023@gmail.com>
2023-10-03 03:15:52 +0900

    feature: order entity mapping

---
+++ circuitbreaker-with-retry/order-api/src/main/java/project/resilience4j/order/core/core/order/entity/Order.java
@@ -0,0 +1,33 @@
+package project.resilience4j.order.core.core.order.entity;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+
+@Getter
+public class Order {
+
+    private Long orderId;
+    private Long userId;
+    private Long productId;
+    private BigDecimal price;
+
+    public Order(
+            Long userId,
+            Long productId,
+            BigDecimal price
+    ) {
+        this.userId = userId;
+        this.productId = productId;
+        this.price = price;
+    }
+
+    public void putPk(Long newPk) {
+        this.orderId = newPk;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(orderId);
+    }
+}
